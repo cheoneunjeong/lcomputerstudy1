d7cfabadc94aa6e275aee1379e3c2928333a144c
minsun1103 <kkang5813@naver.com>
2021-12-03 12:07:11 +0900

    Initial commit

---
+++ src/main/java/com/lcomputerstudy1/example/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.lcomputerstudy1.example.service;
+
+import java.util.List;
+import com.lcomputerstudy1.example.domain.Board;
+
+public interface BoardService {
+	
+	public List<Board> selectBoardList();
+
+}
