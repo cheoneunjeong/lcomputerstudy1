2f4a5b283256a988deeb9be89140da19253263b8
minsun1103 <kkang5813@naver.com>
2021-12-06 11:49:13 +0900

    add-logback

--- src/main/java/com/lcomputerstudy1/example/controller/Controller.java
+++ src/main/java/com/lcomputerstudy1/example/controller/Controller.java
@@ -1,7 +1,10 @@
 package com.lcomputerstudy1.example.controller;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
@@ -15,6 +18,7 @@ import com.lcomputerstudy1.example.service.UserService;
 @org.springframework.stereotype.Controller
 public class Controller {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired UserService userservice;
 	@Autowired BoardService boardservice;
 	
@@ -23,9 +27,12 @@ public class Controller {
 		
 		List<Board> list = boardservice.selectBoardList();
 		model.addAttribute("list", list);
+		logger.debug("debug");
+	     logger.info("info");
+	     logger.error("error");
 		return "/index";
 	}
-	
+
 	@RequestMapping("/beforeSignUp")
 	public String beforeSignUp() {
 		return "/signup";
@@ -53,6 +60,23 @@ public class Controller {
 	@RequestMapping(value="/login")
 	public String beforeLogin(Model model) {
 		return "/login";
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping(value="/admin")
+	public String admin(Model model) {
+		return "/admin";
+	}
+	
+	@Secured({"ROLE_USER"})
+	@RequestMapping(value="/user/info")
+	public String userInfo(Model model) {
+		return "/user_info";
+	}
+	
+	@RequestMapping(value="/denied")
+	public String denied(Model model) {
+		return "/denied";
 	}
 
 }
